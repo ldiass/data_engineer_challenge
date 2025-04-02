69cbcdb2c53684eedeb894557f07285dcbfc20f2
LuisSoaresBM <ldiass@live.com>
2025-04-02 09:40:12 +0200

    Use sum instead of average in gold table

--- data_engineer_challenge/models/gold_sf/battalion_incidents.sql
+++ data_engineer_challenge/models/gold_sf/battalion_incidents.sql
@@ -1,6 +1,7 @@
 
 {{ config(
     materialized='materialized_view',
+    on_schema_change='sync_all_columns'
 ) }}
 
 with source_data as (
@@ -9,29 +10,29 @@ with source_data as (
         battalion
         , date_trunc('month', incident_date::date) AS incident_date_month
         , count(1) as incidents_count 
-        , avg(EXTRACT(epoch FROM (alarm_dttm::timestamp - arrival_dttm::timestamp)) / 60) as avg_minutes_to_arrival
-        , avg(EXTRACT(epoch FROM (alarm_dttm::timestamp - close_dttm::timestamp)) / 60) as avg_minutes_to_close
-        , avg(suppression_units) as avg_suppression_units
-        , avg(suppression_personnel) as avg_suppression_personnel
-        , avg(ems_units) as avg_ems_units
-        , avg(ems_personnel) as avg_ems_personnel
-        , avg(other_units) as avg_other_units
-        , avg(other_personnel) as avg_other_personnel
-        , avg(estimated_property_loss) as avg_estimated_property_loss
-        , avg(estimated_contents_loss) as avg_estimated_contents_loss
-        , avg(fire_fatalities) as avg_fire_fatalities
-        , avg(fire_injuries) as avg_fire_injuries
-        , avg(civilian_fatalities) as avg_civilian_fatalities
-        , avg(civilian_injuries) as avg_civilian_injuries
-        , avg(number_of_alarms) as avg_number_of_alarms
+        , sum(EXTRACT(epoch FROM (arrival_dttm::timestamp - alarm_dttm::timestamp)) / 60) as sum_minutes_to_arrival
+        , sum(EXTRACT(epoch FROM (close_dttm::timestamp - alarm_dttm::timestamp)) / 60) as sum_minutes_to_close
+        , sum(suppression_units) as sum_suppression_units
+        , sum(suppression_personnel) as sum_suppression_personnel
+        , sum(ems_units) as sum_ems_units
+        , sum(ems_personnel) as sum_ems_personnel
+        , sum(other_units) as sum_other_units
+        , sum(other_personnel) as sum_other_personnel
+        , sum(estimated_property_loss) as sum_estimated_property_loss
+        , sum(estimated_contents_loss) as sum_estimated_contents_loss
+        , sum(fire_fatalities) as sum_fire_fatalities
+        , sum(fire_injuries) as sum_fire_injuries
+        , sum(civilian_fatalities) as sum_civilian_fatalities
+        , sum(civilian_injuries) as sum_civilian_injuries
+        , sum(number_of_alarms) as sum_number_of_alarms
         , sum(CASE 
             WHEN detectors_present='1 Present' or detectors_present='1 -Present' 
             THEN 1
         END) as count_detectors_present
-        , avg(number_of_floors_with_minimum_damage) as avg_number_of_floors_with_minimum_damage
-        , avg(number_of_floors_with_significant_damage) as avg_number_of_floors_with_significant_damage
-        , avg(number_of_floors_with_heavy_damage) as avg_number_of_floors_with_heavy_damage
-        , avg(number_of_floors_with_extreme_damage) as avg_number_of_floors_with_extreme_damage
+        , sum(number_of_floors_with_minimum_damage) as sum_number_of_floors_with_minimum_damage
+        , sum(number_of_floors_with_significant_damage) as sum_number_of_floors_with_significant_damage
+        , sum(number_of_floors_with_heavy_damage) as sum_number_of_floors_with_heavy_damage
+        , sum(number_of_floors_with_extreme_damage) as sum_number_of_floors_with_extreme_damage
         , sum(CASE 
             WHEN automatic_extinguishing_system_present='1 Present' 
                 OR automatic_extinguishing_system_present='1 -Present'
